e9610f1c4282670e5003a358b033d888e9d96757
starryeye <see2818@naver.com>
2024-04-20 19:08:11 +0900

    [starryeye, 2024.04.20] - resilience4j circuit breaker example
    내용
    - resilience4j circuit breaker example

--- circuitbreaker/src/main/java/dev/practice/circuitbreaker/config/Resilience4jCircuitBreakerConfig.java
+++ circuitbreaker/src/main/java/dev/practice/circuitbreaker/config/Resilience4jCircuitBreakerConfig.java
@@ -16,20 +16,47 @@ public class Resilience4jCircuitBreakerConfig {
     @Bean
     public RegistryEventConsumer<CircuitBreaker> myRegistryEventConsumer() {
 
+        // https://resilience4j.readme.io/docs/circuitbreaker#consume-emitted-circuitbreakerevents
+
         return new RegistryEventConsumer<>() {
             @Override
             public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
                 log.info("RegistryEventConsumer::onEntryAddedEvent");
-                entryAddedEvent.getAddedEntry()
-                        .getEventPublisher()
-                        .onEvent(
-                                event -> log.info(event.toString())
-                        );
-                entryAddedEvent.getAddedEntry()
-                        .getEventPublisher()
-                        .onFailureRateExceeded(
-                                event -> log.info("{}", event.getEventType())
-                        );
+                CircuitBreaker.EventPublisher eventPublisher = entryAddedEvent.getAddedEntry().getEventPublisher();
+
+                eventPublisher.onEvent( // 서킷 브레이커의 모든 이벤트에 대해 수행할 consumer 등록
+                        event -> log.info("onEvent : {}", event)
+                );
+
+                eventPublisher.onSuccess( // 서킷 브레이커로 수행한 요청이 성공했을 때 수행할 consumer 등록
+                        event -> log.info("onSuccess : {}", event)
+                );
+
+                eventPublisher.onCallNotPermitted( // open 상태의 서킷 브레이커로 수행하여 요청이 차단되었을 때 수행할 consumer 등록
+                        event -> log.info("onCallNotPermitted : {}", event)
+                );
+
+                eventPublisher.onError( // record exception 이 발생했을 때, 수행할 consumer 등록
+                        event -> log.info("onError : {}", event)
+                );
+
+                eventPublisher.onIgnoredError( // ignore exception 이 발생했을 때, 수행할 consumer 등록
+                        event -> log.info("onIgnoredError : {}", event)
+                );
+
+                eventPublisher.onStateTransition( // 서킷 브레이커의 상태가 변경될 때 수행할 consumer 등록
+                        event -> log.info("onStateTransition : {}", event)
+                );
+
+
+
+                eventPublisher.onFailureRateExceeded( // 서킷 브레이커가 실패(record exception)율을 초과하면 수행할 consumer 등록
+                        event -> log.info("onFailureRateExceeded : {}", event)
+                );
+
+                eventPublisher.onSlowCallRateExceeded( // 서킷 브레이커가 실패(timeout)율을 초과하면 수행할 consumer 등록
+                        event -> log.info("onSlowCallRateExceeded : {}", event)
+                );
 
             }
 
